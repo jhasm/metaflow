a6d99f026eae72f37dac2c271609584aed10ee85
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2023-10-03 12:44:14 +0300

    wip: fixed remote execution pip-git-pkg support

--- metaflow/plugins/pypi/bootstrap.py
+++ metaflow/plugins/pypi/bootstrap.py
@@ -114,9 +114,10 @@ if __name__ == "__main__":
                 shutil.move(tmpfile, dest)
 
         # Install PyPI packages.
+        # *.whl for regular index sources, *.zip for VCS sources
         cmds.extend(
             [
-                f"""./micromamba run --prefix {prefix} pip --disable-pip-version-check install --root-user-action=ignore --no-compile {pypi_pkgs_dir}/*.whl"""
+                f"""./micromamba run --prefix {prefix} pip --disable-pip-version-check install --root-user-action=ignore --no-compile {pypi_pkgs_dir}/*"""
             ]
         )
 
